cf84ff841340b0690cd9386458efa943e5a4df20
hyckju <hyckjoo0125@gmail.com>
2025-10-19 23:38:38 +0900

    소스코드 수정

--- src/ScientifiCalculator.java
+++ src/ScientifiCalculator.java
@@ -1,16 +1,16 @@
 class ScientifiCalculator extends AdvancedCalculator {
 
-    public void power() {
+    public int power() {
         if (num2 < 0) {
             System.out.println("음수 거듭제곱은 불가합니다.");
             result = Integer.MAX_VALUE;
-            return;
+            return 0;
         }
 
         if (num2 == 0) {
             result = 1;
             System.out.println(num1 + "^" + num2 + "=" + result);
-            return;
+            return 0;
         }
 
         if (num1 == 0) {
@@ -18,11 +18,13 @@ class ScientifiCalculator extends AdvancedCalculator {
             System.out.println(num1 + "^" + num2 + "=" + result);
         }
 
-        result = 1;
+        int scResult = 1;
 
         for (int i = 0; i < num2; i++) {
-            result *= num1;
+            scResult *= num1;
         }
+        result = scResult;
+        return result;
     }
 
 }
