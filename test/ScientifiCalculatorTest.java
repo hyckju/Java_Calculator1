86cdc5c58e2f0572e02331375acc7b0cedcc876e
hyckju <hyckjoo0125@gmail.com>
2025-10-23 21:02:05 +0900

    테스트코드 수정 후 실행결과 이상없음

--- test/ScientifiCalculatorTest.java
+++ test/ScientifiCalculatorTest.java
@@ -1,10 +1,13 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScientifiCalculatorTest {
 
     @Test
-    void divideMod() {
+    void testPower() {
+        ScientifiCalculator  sci = new ScientifiCalculator();
+        sci.setNumbers(4,2);
+        assertEquals(16,sci.power());
+
     }
 }
