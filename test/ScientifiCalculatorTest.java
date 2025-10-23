dee317afad080f6660b5e4ca29a0cf414a596090
hyckju <hyckjoo0125@gmail.com>
2025-10-23 21:03:00 +0900

    Revert "테스트코드 수정 후 실행결과 이상없음"
    This reverts commit 86cdc5c58e2f0572e02331375acc7b0cedcc876e.

--- test/ScientifiCalculatorTest.java
+++ test/ScientifiCalculatorTest.java
@@ -1,13 +1,10 @@
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScientifiCalculatorTest {
 
     @Test
-    void testPower() {
-        ScientifiCalculator  sci = new ScientifiCalculator();
-        sci.setNumbers(4,2);
-        assertEquals(16,sci.power());
-
+    void divideMod() {
     }
 }
